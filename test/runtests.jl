2712a41e209909e01f1f0ea220eaab6cf9f7e9ff
Neven Sajko <4944410+nsajko@users.noreply.github.com>
2025-10-07 19:13:20 +0200

    add some tests (#5)

--- test/runtests.jl
+++ test/runtests.jl
@@ -2,7 +2,14 @@ using GraphProperties
 using Test
 
 @testset "GraphProperties.jl" begin
-    # Write your tests here.
+    @testset "construction, subtyping" begin
+        @test PropertyComparison(==, Arboricity(), 2) isa GraphProperty{Bool}
+        @test PropertyComparison(<=, Boxicity(), 2) isa GraphProperty{Bool}
+        @test GraphIsChordal() isa GraphProperty{Bool}
+        @test Arboricity() isa GraphProperty{Integer}
+        @test FractionalChromaticNumber() isa GraphProperty{Real}
+        @test DegreeSequence() isa GraphProperty{AbstractVector}
+    end
 end
 
 using Aqua: Aqua
