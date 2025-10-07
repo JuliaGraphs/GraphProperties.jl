016503763aa47d809d8802cf1076a2efcbad6925
Documenter.jl <documenter@juliadocs.github.io>
2025-10-07 11:39:45 +0000

    build based on 29e63a9

---
+++ dev/search_index.js
@@ -0,0 +1,3 @@
+var documenterSearchIndex = {"docs":
+[{"location":"#GraphProperties","page":"Home","title":"GraphProperties","text":"","category":"section"},{"location":"","page":"Home","title":"Home","text":"Documentation for GraphProperties.","category":"page"},{"location":"","page":"Home","title":"Home","text":"","category":"page"}]
+}
